84f8c6a7750c5f388c52f2298a6a76a4bba905de
Fabricio C Santos <fsantos.veredinha@gmail.com>
2024-05-13 19:42:33 -0300

    Classes

---
+++ contatos/src/main/java/br.com.fiap.contatos/model/Contato.java
@@ -0,0 +1,72 @@
+package br.com.fiap.contatos.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TBL_CONTATOS")
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CONTATO")
+    @SequenceGenerator(name = "SEQ_CONTATO", initialValue = 1, allocationSize = 1)
+    @Column(name = "ID_CONTATO")
+    private Long id;
+
+    @Column(name = "NM_CONTATO")
+    private String nome;
+
+    @Column(name = "EM_CONTATO")
+    private String email;
+
+    @Column(name = "DTNASC_CONTATO")
+    private LocalDate dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(id, contato.id) && Objects.equals(nome, contato.nome) && Objects.equals(email, contato.email) && Objects.equals(dataNascimento, contato.dataNascimento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, dataNascimento);
+    }
+
+}

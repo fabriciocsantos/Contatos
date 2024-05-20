9419b9b1573920a1cdce9ed8e3c1fa017b41df41
Fabricio C Santos <fsantos.veredinha@gmail.com>
2024-05-20 19:34:16 -0300

    Spring Security

--- contatos/src/main/java/br.com.fiap.contatos/controller/ContatoController.java
+++ contatos/src/main/java/br.com.fiap.contatos/controller/ContatoController.java
@@ -8,8 +8,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
--- contatos/src/main/java/br.com.fiap.contatos/repository/ContatoRepository.java
+++ contatos/src/main/java/br.com.fiap.contatos/repository/ContatoRepository.java
@@ -4,10 +4,6 @@ import br.com.fiap.contatos.model.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface ContatoRepository extends JpaRepository<Contato, Long> {
